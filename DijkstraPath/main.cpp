c18a92ffba7fff7d8485a0a491694cb01c27b861
e0035165 <eeenginmaster95@gmail.com>
2023-08-03 10:18:55 +0800

    Initial Commit

---
+++ DijkstraPath/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DijkstraPath
+//
+//  Created by Sart Way on 3/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
